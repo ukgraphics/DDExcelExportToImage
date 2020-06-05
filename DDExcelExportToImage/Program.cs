1706e7e166360826776daff6b73a32dab570a75e
Kuni <kuni.ujiie@grapecity.com>
2020-06-05 16:32:21 +0900

    Updated.

--- DDExcelExportToImage/Program.cs
+++ DDExcelExportToImage/Program.cs
@@ -1,6 +1,4 @@
 ﻿using GrapeCity.Documents.Excel;
-using GrapeCity.Documents.Excel.Drawing;
-using System.IO;
 
 namespace DDExcelExportToImage
 {
@@ -8,31 +6,31 @@ namespace DDExcelExportToImage
     {
         static void Main(string[] args)
         {
-			// ライセンスを設定します
-			//Workbook.SetLicenseKey("トライアル版もしくは製品版のライセンスキー");
+            // ライセンスを設定します
+            //Workbook.SetLicenseKey("トライアル版もしくは製品版のライセンスキー");
 
-			// 新規ワークブックの作成
-			var workbook = new Workbook();
+            // 新規ワークブックの作成
+            var workbook = new Workbook();
 
-			// xlsxファイルを開く
-			workbook.Open("Template_SalesTracker_report.xlsx");
-			IWorksheet worksheet = workbook.Worksheets[0];
+            // xlsxファイルを開く
+            workbook.Open("Template_SalesTracker_report.xlsx");
+            IWorksheet worksheet = workbook.Worksheets[0];
 
-			// ワークシートを画像に出力
-			worksheet.ToImage("worksheet.png");
+            // ワークシートを画像に出力
+            worksheet.ToImage("worksheet.png");
 
-			// セル範囲を画像に出力
-			worksheet.Range["B15:K21"].ToImage("range.png");
+            // セル範囲を画像に出力
+            worksheet.Range["B15:K21"].ToImage("range.png");
 
-			// シェイプを画像に出力
-			worksheet.Shapes["TextBox"].ToImage("textbox.png");
+            // シェイプを画像に出力
+            worksheet.Shapes["TextBox"].ToImage("textbox.png");
 
-			// バーチャートを画像に出力
-			worksheet.Shapes["ProductIncomeChart"].ToImage("barchart.png");
+            // バーチャートを画像に出力
+            worksheet.Shapes["ProductIncomeChart"].ToImage("barchart.png");
 
-			// パイチャートを画像に出力
-			worksheet.Shapes["ProductIncomePctChart"].ToImage("piechart.png");
+            // パイチャートを画像に出力
+            worksheet.Shapes["ProductIncomePctChart"].ToImage("piechart.png");
 
-		}
-	}
+        }
+    }
 }
