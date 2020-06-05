68ee466c3215b89a21787d0c66d39b74a0246be2
Kuni <kuni.ujiie@grapecity.com>
2020-06-05 16:29:52 +0900

    プロジェクト ファイルを追加します。

---
+++ DDExcelExportToImage/Program.cs
@@ -0,0 +1,38 @@
+﻿using GrapeCity.Documents.Excel;
+using GrapeCity.Documents.Excel.Drawing;
+using System.IO;
+
+namespace DDExcelExportToImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// ライセンスを設定します
+			//Workbook.SetLicenseKey("トライアル版もしくは製品版のライセンスキー");
+
+			// 新規ワークブックの作成
+			var workbook = new Workbook();
+
+			// xlsxファイルを開く
+			workbook.Open("Template_SalesTracker_report.xlsx");
+			IWorksheet worksheet = workbook.Worksheets[0];
+
+			// ワークシートを画像に出力
+			worksheet.ToImage("worksheet.png");
+
+			// セル範囲を画像に出力
+			worksheet.Range["B15:K21"].ToImage("range.png");
+
+			// シェイプを画像に出力
+			worksheet.Shapes["TextBox"].ToImage("textbox.png");
+
+			// バーチャートを画像に出力
+			worksheet.Shapes["ProductIncomeChart"].ToImage("barchart.png");
+
+			// パイチャートを画像に出力
+			worksheet.Shapes["ProductIncomePctChart"].ToImage("piechart.png");
+
+		}
+	}
+}
